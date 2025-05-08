94d17e047b99674e0bc5ab1bee47bab4f7312b23
monet2155 <monet9812@gmail.com>
2025-05-08 15:46:30 +0900

    refactor: state 분리

--- lib/controller/game_screen_controller.dart
+++ lib/controller/game_screen_controller.dart
@@ -9,66 +9,7 @@ import 'package:loveloveraid/constants/game_constants.dart';
 import 'package:loveloveraid/exceptions/game_exception.dart';
 import 'package:loveloveraid/services/game_api_service.dart';
 import 'package:loveloveraid/services/tts_service.dart' as tts;
-
-class GameScreenState {
-  final List<DialogueLine> dialogueQueue;
-  final bool isDialoguePlaying;
-  final bool isWaitingForTap;
-  final String visibleText;
-  final DialogueLine? currentLine;
-  final bool isLoading;
-  final String? sessionId;
-  final Set<String> appearedCharacters;
-  final Set<String> animatedCharacters;
-  final List<DialogueLine> dialogueHistory;
-  final int currentHistoryIndex;
-  final bool isInHistoryView;
-
-  GameScreenState({
-    this.dialogueQueue = const [],
-    this.isDialoguePlaying = false,
-    this.isWaitingForTap = false,
-    this.visibleText = '',
-    this.currentLine,
-    this.isLoading = false,
-    this.sessionId,
-    this.appearedCharacters = const {},
-    this.animatedCharacters = const {},
-    this.dialogueHistory = const [],
-    this.currentHistoryIndex = -1,
-    this.isInHistoryView = false,
-  });
-
-  GameScreenState copyWith({
-    List<DialogueLine>? dialogueQueue,
-    bool? isDialoguePlaying,
-    bool? isWaitingForTap,
-    String? visibleText,
-    DialogueLine? currentLine,
-    bool? isLoading,
-    String? sessionId,
-    Set<String>? appearedCharacters,
-    Set<String>? animatedCharacters,
-    List<DialogueLine>? dialogueHistory,
-    int? currentHistoryIndex,
-    bool? isInHistoryView,
-  }) {
-    return GameScreenState(
-      dialogueQueue: dialogueQueue ?? this.dialogueQueue,
-      isDialoguePlaying: isDialoguePlaying ?? this.isDialoguePlaying,
-      isWaitingForTap: isWaitingForTap ?? this.isWaitingForTap,
-      visibleText: visibleText ?? this.visibleText,
-      currentLine: currentLine ?? this.currentLine,
-      isLoading: isLoading ?? this.isLoading,
-      sessionId: sessionId ?? this.sessionId,
-      appearedCharacters: appearedCharacters ?? this.appearedCharacters,
-      animatedCharacters: animatedCharacters ?? this.animatedCharacters,
-      dialogueHistory: dialogueHistory ?? this.dialogueHistory,
-      currentHistoryIndex: currentHistoryIndex ?? this.currentHistoryIndex,
-      isInHistoryView: isInHistoryView ?? this.isInHistoryView,
-    );
-  }
-}
+import 'package:loveloveraid/model/game_screen_state.dart';
 
 class GameScreenController {
   final String playerName;

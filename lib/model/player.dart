71fb6f3fecaef5cd09fa092dd4faba409e6a5935
monet2155 <monet9812@gmail.com>
2025-06-11 15:58:49 +0900

    feat: affectionScore at Player

--- lib/model/player.dart
+++ lib/model/player.dart
@@ -1,6 +1,11 @@
 class Player {
   String id;
   String name;
+  Map<String, int> affectionScore = {};
 
   Player({this.id = '', this.name = ''});
+
+  void addAffectionScore(String npcId, int score) {
+    affectionScore[npcId] = (affectionScore[npcId] ?? 0) + score;
+  }
 }
